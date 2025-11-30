2ae2e08e475bf350784f029dabb0899490bb5d52
Pieash Ahmed <pieash9@gmail.com>
2025-11-30 21:59:47 +0600

    fib series

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 
 fn main() {
-    let ans: bool = is_even(1000000000001);
-    println!( "{}",ans);
+    println!("{}", fib(10));
 }
 
-fn is_even(num: i64) -> bool {
-    if num % 2 == 0 {
-        return  true;
-    }else {
-        return  false;
+fn fib(num: u32) -> u32 {
+    let mut first = 0;
+    let mut second = 1;
+
+    if num == 0 {
+        return first;
     }
+    if num == 1 {
+        return second;
+    }
+
+    for _ in 0..(num - 1) {
+        let temp = second;
+        second = first + second;
+        first = temp;
+    } 
+
+    second
+
 }
