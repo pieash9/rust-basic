a920685e48e06f23d33438dde403754f8a36e636
Pieash Ahmed <pieash9@gmail.com>
2025-12-01 22:17:59 +0600

    Refactor User struct and update main function to create and display user details

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-
-fn main() {
-    let name = String::from("Pieash");
-    let length = get_str_length(name);
-    println!("Length of the string is: {}", length);
+struct User{
+    first_name: String,
+    last_name: String,
+    age: i32,
 }
 
-fn get_str_length(str: String) -> usize {
-   return str.chars().count();
+
+fn main() {
+    let user =  User {
+        first_name: String::from("Pieash"),
+        last_name: String::from("Khan"),
+        age: 23,
+    };
+
+    println!("User: {} {}, Age: {}", user.first_name, user.last_name, user.age);
+
 }
