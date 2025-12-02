9781328ba0a8dcd553c6f279d27fa7166370b3a0
Pieash Ahmed <pieash9@gmail.com>
2025-12-02 22:13:06 +0600

    Refactor shape representation by replacing Rect struct with Shape enum and updating area calculation

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
-struct Rect {  // like js class
-    width: u32, // like js class property
-    height: u32, // like js class property
-}
-
-impl Rect {
-    fn area(&self) -> u32 {  // this is like js instance method
-        self.width * self.height
-    }
-
-    fn perimeter(&self) -> u32 {
-        2 * (self.width + self.height)
-    }
-
-    fn debug() -> u32 {   // this is like js static method
-        return  1;
-    }
+enum  Shape {
+    Rectangle(f64, f64),
+    Circle(f64)
 }
 
 fn main() {
-    let rect1 = Rect {width: 88, height: 19}; // like js new class()
+   let react = Shape::Rectangle(1.0, 2.0);
+   calculate_area(react);
 
-    println!("The area of the rectangle is {} square pixels.", rect1.area());
-    println!("The perimeter of the rectangle is {} pixels.", rect1.perimeter());
-    println!("The debug of the rectangle is {} pixels.", Rect::debug());
+   let circle = Shape::Circle( 2.0);
+   calculate_area(circle);
+}
 
+fn calculate_area(shape: Shape) -> f64 {
+   let area =  match shape {
+        Shape::Rectangle(a,b ) => a * b,
+        Shape::Circle(r) => 3.14 * r * r,
+    };
+
+    return area;
 }
