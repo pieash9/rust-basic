16c3ed65339dbdcd4ce9e997f7a822c6aebb1218
Md. Pieash Ahmed <pieash9@gmail.com>
2025-12-04 18:12:10 +0600

    Update dependencies and refactor main function to display current UTC time

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
-use std::fs::read_to_string;
+use chrono::Utc;
 
-fn main(){
-     let result = read_from_file(String::from("a.txt"));
-    println!("{}", result.unwrap());
-   
-}
-
-fn read_from_file(file_path: String) -> Result<String, String> {
-     let result = read_to_string(file_path);
-
-    match result {
-        Ok(data) => Ok(data),
-        Err(_err) => panic!("Error reading file"),
-    }
+fn main() {
+    let now  = Utc::now();
+    println!("Current UTC time: {}", now);
 }
