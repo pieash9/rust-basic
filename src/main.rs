b569d1308ee438ce12ce2c4559c1ef58e5c6daea
Pieash Ahmed <pieash9@gmail.com>
2025-12-05 20:41:42 +0600

    Add local time display to main function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use chrono::Utc;
+use chrono::{Local, Utc};
 
 fn main() {
     let now  = Utc::now();
+    let local_time = Local::now();
+    println!("Current Local time: {}", local_time);
     println!("Current UTC time: {}", now);
 }
