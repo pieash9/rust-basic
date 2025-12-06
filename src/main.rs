a2fb2d49727c39e6dc6d020a2220f2b5641c0041
Pieash Ahmed <pieash9@gmail.com>
2025-12-06 21:54:18 +0600

    Refactor main function to correctly demonstrate mutable borrowing in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     // ownership.. only one owner at a time
     let mut s1 = String::from("Pieash");
-    let s2 = s1;
-    s1 = s2;
+    let s2 = &mut s1;
+    s2.push_str("Ahmed");
+    println!("{:p}", &s2);
     println!("{}", s1);
 }
 
