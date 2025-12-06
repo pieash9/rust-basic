8f3d39efe9ade73c98fe9b1a817d6f382675f91e
Pieash Ahmed <pieash9@gmail.com>
2025-12-06 22:16:35 +0600

    Refactor main function to remove ownership example and implement even number filtering from a vector

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 fn main() {
-    // ownership.. only one owner at a time
-    let mut s1 = String::from("Pieash");
-    let s2 = &mut s1;
-    s2.push_str("Ahmed");
-    println!("{:p}", &s2);
-    println!("{}", s1);
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    vec.push(3);
+    vec.push(24);
+    vec.push(55);
+
+    println!("Vector contents: {:?}", even_filter(vec));
 }
 
+fn even_filter (vec : Vec<i32>) -> Vec<i32> {
+    let mut new_vec = Vec::new();
+
+    for val in vec {
+        if val % 2 == 0{
+            new_vec.push(val);
+        }
+    }
+
+    return new_vec;
+}
