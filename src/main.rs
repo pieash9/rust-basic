ecd5daa41b3f4006d025ed3672ea9cd20518bcb8
Md. Pieash Ahmed <pieash9@gmail.com>
2025-12-07 14:27:49 +0600

    iterator

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use std::collections::HashMap;
-
-fn group_values_by_keys(vec : Vec<(String, i32)>) -> HashMap<String, i32> {
-    let mut hm = HashMap::new();
-    for (key, value) in vec {
-        hm.insert(key, value);
-    }
-    return  hm;
-}
-
-
 fn main() {
-    let input_vec = vec![(String::from("pieash"), 22), (String::from("apple"), 10), (String::from("banana"), 15)];
-    let hm = group_values_by_keys(input_vec);
-    println!("{:?}", hm);
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+
+    println!("v1 still owns its data: {:?}", v1);
 }
