6f2c799a7d06aec29c22c197395e488102cedeb5
Md. Pieash Ahmed <pieash9@gmail.com>
2025-12-08 16:44:30 +0600

    iter mutable

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 fn main() {
     let mut v1 = vec![1, 2, 3];
-    let mut v1_iter = v1.iter_mut();
+    let  v1_iter = v1.iter_mut();
 
-    let first_number = v1_iter.next();
-    let second_number = v1_iter.next();
-    let third_number = v1_iter.next();
-
-    // while let Some(val) = v1_iter.next() {
-    //     print!("{}", val);
-    // }
-
+    for val in v1_iter {
+        *val += 1;
+    }
+ 
     println!("{:?}", v1);
 }
