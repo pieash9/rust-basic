1fbec360579a4f3b727ad23c2566b70870ebfc24
Pieash Ahmed <pieash9@gmail.com>
2025-12-16 22:37:22 +0600

    default implementations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+
+trait Summary {
+    fn summaries(&self) -> String;
+}
+
+struct User {
+    name : String,
+    age: u32,
+}
+
+impl Summary for User {
+    fn summaries(&self) -> String {
+        return  format!("The name is {}, and the age is {}", self.name, self.age );
+    }
+}
+
 fn main() {
-    let v = [1,2,3,4];
-    println!("{:?}", &v[0..1])
+    let user = User{
+        name:String::from("Pieash"),
+        age: 27
+    };
+    println!("{}", user.summaries());
 }
